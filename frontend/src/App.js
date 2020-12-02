2d16a7ff8e3c847b7338e1404aab8110f8e1fb0d
Jonathan R <2140173@gmail.com>
2020-12-03 00:19:52 +0200

    section 2: lesson 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
-    <>
-      <h1>Welcom To Proshop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
